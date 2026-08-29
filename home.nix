90963cea3c2661b795df5bfed928392f2698cc56
Francis Usher <francis.w.usher@gmail.com>
2026-08-28 23:47:53 -0700

    Add bat/ripgrep/fzf/zoxide/yazi via Home Manager, wired into bash
    Ports the QoL tool configs from the Arch .zshrc (minus oh-my-posh and
    the zsh switch itself, which are separate follow-ups).

--- home.nix
+++ home.nix
@@ -4,4 +4,31 @@
   home.username = "soong";
   home.homeDirectory = "/home/soong";
   home.stateVersion = "24.11";
+
+  programs.bash.enable = true;
+  programs.bat.enable = true;
+  programs.ripgrep.enable = true;
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+    options = [ "--cmd cd" ];
+  };
+  programs.yazi = {
+    enable = true;
+    enableBashIntegration = true;
+    shellWrapperName = "y";
+  };
+
+  home.shellAliases = {
+    cat = "bat";
+    grep = "rg";
+  };
+  home.sessionVariables = {
+    MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+    MANROFFOPT = "-c";
+  };
 }
